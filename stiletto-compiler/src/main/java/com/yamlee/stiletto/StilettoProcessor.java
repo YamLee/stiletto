8284f8225d0f5a92bd83604619645e0ca534e787
YamLee <liyaninhn@gmail.com>
2017-03-09 16:58:37 +0800

    Add stiletto Sample module

--- stiletto-compiler/src/main/java/com/yamlee/stiletto/StilettoProcessor.java
+++ stiletto-compiler/src/main/java/com/yamlee/stiletto/StilettoProcessor.java
@@ -1,11 +1,24 @@
 package com.yamlee.stiletto;
 
-import com.sun.xml.internal.stream.buffer.AbstractProcessor;
+
+import com.google.auto.service.AutoService;
+
+import java.util.Set;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Processor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.element.TypeElement;
 
 /**
  * Annotation Processor
  *
  * @author yamlee
  */
+@AutoService(Processor.class)
 public class StilettoProcessor extends AbstractProcessor {
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        return false;
+    }
 }
--- stiletto/src/main/java/com/yamlee/stiletto/Stiletto.java
+++ stiletto/src/main/java/com/yamlee/stiletto/Stiletto.java
@@ -1,9 +1,40 @@
 package com.yamlee.stiletto;
 
+import android.app.Activity;
+import android.app.Fragment;
+import android.support.annotation.NonNull;
+import android.support.annotation.UiThread;
+
 /**
  * Stiletto with public static method
  *
  * @author yamlee
  */
 public class Stiletto {
+
+    private Stiletto() {
+        throw new AssertionError("No instances.");
+    }
+
+    /**
+     * Inject method for Activity,call this method will auto inject param which
+     * annotated with {@link InjectParam} with Intent extra value
+     *
+     * @param activity target activity for param injection
+     */
+    @UiThread
+    public static void inject(@NonNull Activity activity) {
+
+    }
+
+    /**
+     * Inject method for Fragment,call this method will auto inject param which
+     * annotated with {@link InjectParam} with Intent extra value
+     *
+     * @param fragment target fragment for param injection
+     */
+    @UiThread
+    public static void inject(@NonNull Fragment fragment) {
+
+    }
 }
